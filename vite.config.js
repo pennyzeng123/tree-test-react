d92328d054ec5d974059f9db9fe0a665c4c6a1e5
pennyzeng123 <164979045+pennyzeng123@users.noreply.github.com>
2026-04-19 16:17:54 +0800

    deploy docs
    deploy docs

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/tree-test-react/',
   plugins: [react()],
+  base: '/tree-test-react/',
+  build: {
+    outDir: 'docs',
+  },
 })
